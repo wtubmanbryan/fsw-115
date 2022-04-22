7b91bc7c9c670a08dc2e9a15f56636ee0d24f5b7
wtubmanbryan <william.tubman@bryanuniversity.edu>
2022-04-22 01:32:05 -0400

    Added Axios Todo

---
+++ wk4/script.js
@@ -0,0 +1,32 @@
+
+const createLi = (todoList) => {
+    const li = document.createElement('li');
+    // add user details to `li`
+    li.textContent = `ID: ${todoList._id} User Name: ${todoList.user} Description: ${todoList.description} IS Complete: ${todoList.isComplete}`;
+    if(todoList.isComplete == 'yes')
+    {
+        li.style.textDecoration = "line-through"
+    }
+    return li;
+};
+
+const appendToDOM = (todo) => {
+    const ul = document.querySelector('ul');
+    //iterate over all users
+    todo.map(todoList => {
+        ul.appendChild(createLi(todoList));
+        
+    });
+};
+
+const fetchUsers = () => {
+    axios.get('http://api.bryanuniversity.edu/wtubman/list')
+        .then(response => {
+            const todo = response.data;
+            // append to DOM
+            appendToDOM(todo);
+        })
+        .catch(error => console.error(error));
+};
+
+fetchUsers();
